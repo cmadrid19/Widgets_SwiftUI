c389c9c7f7a863f1598a7daed28b19420befc7e5
Max <maxim.macari.dam@gmail.com>
2020-10-19 17:24:19 +0200

    Initial Commit

---
+++ Widget/WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetApp.swift
+//  Widget
+//
+//  Created by Maxim Macari on 19/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
